2e390656458129dcd40b861809cc3821cf3506ba
Sergey Skorokhod <sd.skorokhod@vebtech.by>
2026-04-16 12:02:31 +0300

    update postgres migration script

--- include/db/migrations/01_init.sql
+++ include/db/migrations/01_init.sql
@@ -1,6 +1,16 @@
+-- 1. Таблица Пользователей
 CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     status VARCHAR(50) NOT NULL
-)
+);
+
+-- 2. Таблица Отзывов
+CREATE TABLE IF NOT EXISTS reviews (
+    id VARCHAR(255) PRIMARY KEY,
+    text TEXT NOT NULL,
+    sentiment VARCHAR(50),
+    summary TEXT,
+    status VARCHAR(50) NOT NULL
+);
